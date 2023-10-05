df40e0d426ef29b52b8ee7701eef7a728bf34dc9
ranjanraj343 <ranjannitk.93@gmail.com>
2023-10-05 23:40:56 +0530

    Cucumber Project files

---
+++ src/test/java/stepDefinitions/Base.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObject.AddNewCustomerPage;
+import pageObject.AdminPage;
+import utilities.ReadConfig;
+
+public class Base {
+
+ public	WebDriver driver;
+	
+ public AdminPage ad;
+ 
+ public  ReadConfig readConfig;
+ 
+ public AddNewCustomerPage addCust;
+	
+}
